c03fc379180efab3e1b504ac1a114c55163e5476
luigicara <luigi.caravelli98@gmail.com>
2023-04-14 18:11:16 +0200

    Add project files.

---
+++ Indirizzo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Zip { get; set; }
+
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            if (name == null || name == String.Empty)
+                throw new ArgumentNullException(nameof(name), $"Impossibile creare Indirizzo senza");
+            else
+                Name = name;
+
+            if (surname == null || surname == String.Empty)
+                throw new ArgumentNullException(nameof(surname), $"Impossibile creare Indirizzo senza");
+            else
+                Surname = surname;
+
+            if (street == null || street == String.Empty)
+                throw new ArgumentNullException(nameof(street), $"Impossibile creare Indirizzo senza");
+
+            if (city == null || city == String.Empty)
+                throw new ArgumentNullException(nameof(city), $"Impossibile creare Indirizzo senza");
+            else
+                City = city;
+
+            if (province == null || province == String.Empty)
+                throw new ArgumentNullException(nameof(province), $"Impossibile creare Indirizzo senza");
+            else
+                Province = province;
+
+            if (zip == null || zip == String.Empty)
+                throw new ArgumentNullException(nameof(zip), $"Impossibile creare Indirizzo senza");
+            else
+                Zip = zip;
+
+            if (!char.IsDigit(street[0]))
+                throw new Exception($"Il campo {nameof(street)} deve iniziare per numero");
+            else
+                Street = street;
+
+
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Surname + ", " + Street + ", " + City + " (" + Province + ") " + Zip;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace csharp_lista_indirizzi
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<Indirizzo> indirizzi =  new List<Indirizzo>();
+
+            StreamReader reader = File.OpenText("dati\\addresses.csv");
+
+            int index = 0;
+            while (!reader.EndOfStream)
+            {
+          
+                try
+                {
+                    string? line = reader.ReadLine();
+
+                    if (line != null && index != 0)
+                    {
+                        string[] valori = line.Split(',');
+
+                        Console.WriteLine($"UTENTE: {index}");
+                        if (valori.Length != 6)
+                            throw new Exception("Il numero di valori inseriti non corrisponde ai campi richiesti");
+                        else
+                        {
+                            Console.WriteLine($"\tNome: {valori[0]}");
+                            Console.WriteLine($"\tCognome: {valori[1]}");
+                            Console.WriteLine($"\tVia: {valori[2]}");
+                            Console.WriteLine($"\tCittà: {valori[3]}");
+                            Console.WriteLine($"\tProvincia: {valori[4]}");
+                            Console.WriteLine($"\tCAP: {valori[5]}");
+
+                            indirizzi.Add(new Indirizzo(valori[0], valori[1], valori[2], valori[3], valori[4], valori[5]));
+                        }
+                    }
+                    index++;
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("Si è verificato un problema: " + ex.Message);
+
+                    index++;
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Si è verificato un problema: " + e.Message);
+
+                    index++;
+                }
+
+            }
+
+            reader.Close();
+
+            foreach (Indirizzo indirizzo in indirizzi)
+                Console.WriteLine(indirizzo.ToString());
+        }
+    }
+}
